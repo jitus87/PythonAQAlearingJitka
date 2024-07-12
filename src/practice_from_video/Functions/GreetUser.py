51213e90890a30b8db47a2539fe4249af339e9ea
Jitka <jitka.czippelova@gmail.com>
2024-07-12 16:15:59 +0200

    Programs - based on tutorial video - Functions

---
+++ src/practice_from_video/Functions/GreetUser.py
@@ -0,0 +1,9 @@
+def greet_user(name):
+    print(f"Hi {name}!")
+    print("Welcome abroad")
+
+
+print("Start")
+greet_user("John")
+greet_user("Mary")
+print("Finish")
---
+++ src/practice_from_video/Functions/Return.py
@@ -0,0 +1,8 @@
+# returns a value as a result of a function, which can be used in the code
+
+def square(number):
+    return number*number
+
+
+result = square(3)
+print(result)               # or we can use only:   print(square(3))
