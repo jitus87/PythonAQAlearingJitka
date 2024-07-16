d24bcbf55196ca72f4edb81c4321c033bd56840b
Jitka <jitka.czippelova@gmail.com>
2024-07-16 15:20:18 +0200

    Programs - based on tutorial video - Exceptions and Classes

---
+++ applicationForPackages1.py
@@ -0,0 +1,5 @@
+#we import whole package with it' s module
+import ecommerce.shipping
+
+print(ecommerce.shipping.calc_shipping())
+print(ecommerce.shipping.calc_tax())
---
+++ ecommerce/shipping.py
@@ -0,0 +1,6 @@
+def calc_shipping():
+    print("cals_shipping")
+
+
+def calc_tax():
+    print("calc_tax")
